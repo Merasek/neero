d479e65fe62bd60f791730842bf92560ee15d0dc
Nikolai <45694043+Merasek@users.noreply.github.com>
2018-12-30 23:44:58 +0200

    Add files via upload

--- Neero-Hishuviut/src/exe1/neurons.java
+++ Neero-Hishuviut/src/exe1/neurons.java
@@ -1,44 +1,48 @@
 package exe1;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class neurons {
 
-	
+
 	public void matrix (){
 		int [][] mat = new int [10][10];
 		Random random = new Random ();
-		int count_e=0,count_i=0, count_n=0;
-		
-		String [] results = {"Inhibitory", "not conected", "excitatory"};
-		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat[i].length; j++) {
-				mat[i][j]=random.nextInt(3)-1;
-				System.out.print(" " + mat[i][j]);
-				switch(mat[i][j]){
-					case -1: 
-						count_i++;
-						break;
-					case 0: 
-						count_n++;
-						break;
-					case 1: 
-						count_e++;
-						break;
-					default :
-					break;
-				}
-			}
-				System.out.println();
+		int count_e=0,count_i=0;
+
+		int [] results = {-1, 1};
+		for (int i = 0; i < 10; i++) {
+			int x = random.nextInt(10);
+			int y = random.nextInt(10);
+			int ran = random.nextInt(2);
+			if(ran == 0)
+				mat[x][y]=-1;
+			else
+				mat[x][y] = ran;
 		}
-		System.out.println("Inhibitory = "+ count_i +"\nnot conected = " + count_n +"\nexcitatory = "+count_e);
-		
+		int[] arr = new int[10];
+		int sum = 0;
+		for (int i = 0; i < 10; i++) {
+			int x = random.nextInt(10);
+			int y = random.nextInt(10);
+			arr[i] = mat[x][y];
+			sum += mat[x][y];
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + ", ");
+		}
+		System.out.println();
+		if(sum > 0 ) System.out.println("sum "+  1);
+		else System.out.println("sum " + 0);
+		for (int j = 0; j < mat.length; j++) 
+			System.out.println(Arrays.toString(mat[j]));
+
 	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		neurons tt = new neurons();
 		tt.matrix();
 	}
--- Neero-Hishuviut/src/exe1/spike.java
+++ Neero-Hishuviut/src/exe1/spike.java
@@ -1,12 +1,12 @@
 package exe1;
 
-import java.util.Random;
+import java.util.*;
 
 public class spike {
 
 
-	public void bucket (){
-		int leak = -170;
+	public void bucket_A (){
+		int like = -170;
 		int MAX_CAPASITY = 1500;
 		int capasity = 0;
 		int input = 340;
@@ -14,6 +14,7 @@ public class spike {
 		int spike = 0;
 
 		int n = 10000;
+
 		while(n >0){
 			Random random = new Random();
 			int r = random.nextInt(3)+1;
@@ -21,12 +22,14 @@ public class spike {
 				capasity +=input; 
 				if(capasity > MAX_CAPASITY){
 					spike ++;
-					capasity = 0;
+					//System.out.println(spike + " spike*******************");
+
+					capasity -= input;  //capasity =0;
 				}
 
 			}
 			if(capasity > 0)
-				capasity = capasity + leak;
+				capasity = capasity + like;
 			System.out.println("capasity end: " + capasity);
 			n--;
 		}
@@ -34,7 +37,7 @@ public class spike {
 	}
 
 
-	public void bucket2(){
+	public void bucket_B(){
 		int[] taps = new int[2];
 		int[] randoms = new int[taps.length];
 		int input = 100;
@@ -43,7 +46,7 @@ public class spike {
 		int MAX_CAPASITY = 2000;
 		int capasity = 0;
 
-		int n = 20;//time
+		int n = 100;//time
 
 		Random random = new Random();
 		for (int i = 0; i < taps.length; i++) {
@@ -58,7 +61,7 @@ public class spike {
 					capasity +=input; 
 					if(capasity > MAX_CAPASITY){
 						spike ++;
-						capasity = 0;
+						capasity -=input; //capasity = 0;
 					}
 				}
 			}
@@ -71,13 +74,87 @@ public class spike {
 		System.out.println("SPIKE: " + spike);
 	}
 
+	public void bucket_C(){
+		int[] taps = new int[2];
+		int spike = 0;
+		int n = 100000;//time
+
+		LinkedList<Integer> check_5 = new LinkedList<>(); 
+		LinkedList<Integer> check_3 = new LinkedList<>(); 
+
+		Random random = new Random();
+		taps[0] = 3;
+		taps[1] = 5;
+
+		while(n >0){
+			int random_3 = random.nextInt(5)+1;
+			int random_5 = random.nextInt(5)+1;
+			if(random_3 == 3)
+				check_3.addFirst(1);
+			else 
+				check_3.addFirst(0);
+			if(random_5 == 5)
+				check_5.addFirst(1);
+			else
+				check_5.addFirst(0);
+			System.out.println(check_3.toString() + "            " + check_5.toString());
+
+			int flag = 15; //interval
+			boolean counter_3 = false;
+			boolean counter_5 = false;
+			if(check_3.size() >= 3 && check_5.size() >= 5){
+				for (int i = check_3.size(); i > 2 && check_3.size() <= flag; i--) {
+					if(check_3.get(i-1) == 1)
+						if(check_3.get(i-2) == 1)
+							if(check_3.get(i-3) == 1){
+								counter_3 = true;
+								break;
+							}
+				}
+				if(counter_3){
+					for (int i = check_5.size(); i > 4 && check_5.size() <= flag; i--) {
+						if(check_5.get(i-1) == 1)
+							if(check_5.get(i-2) == 1)
+								if(check_5.get(i-3) == 1)
+									if(check_5.get(i-4) == 1)
+										if(check_5.get(i-5) == 1){
+											counter_5 = true;
+											break;
+										}
+					}
+				}
+			}
+
+			if(check_3.size() >= flag)
+				check_3.removeLast();
+			if(check_5.size() >= flag)
+				check_5.removeLast();
+			if(counter_3 && counter_5){
+				spike++;
+				for (int i = 0;  check_3.size()!=2; i++) 
+					check_3.removeLast();
+				for (int i = 0;  check_5.size()!=4; i++) 
+					check_5.removeLast();
+				System.out.println("**** here was a spike ****");
+			}
+
+			n--;
+		}
+
+		System.out.println("SPIKE: " + spike);
+
+	}
+
+
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		spike sp = new spike();
-		//sp.bucket();
-		sp.bucket2();
+		//sp.bucket_A();
+		//sp.bucket_B();
+		sp.bucket_C();
 	}
 
 }
